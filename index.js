d7719c35c57234d708cd4da34d58506ed03c226e
jamie.cornick <jamie.cornick@keyin.com>
2022-10-19 13:14:05 -0230

    Added support for POST and PUT

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-//app.get('/users', db.getUsers)
-app.get('/users/', db.getUserById)
-//app.get('/user/:id', db.getUserById)
+app.get('/users', db.getUsers)
+app.get('/user/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
