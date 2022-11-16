13da1136ab73920a6ae96fd34b4ed64f1e6bb93f
jamie.cornick <jamie.cornick@keyin.com>
2022-11-16 13:04:40 -0330

    Adding mongodb support.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
+const dbo = require('./db/conn')
 const port = 3000
 
 app.use(bodyParser.json())
@@ -21,6 +22,21 @@ app.get('/search', db.findUsersBySearch)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
+
+app.get('/listings', async function (req, res) {
+      const dbConnect = dbo.getDb();
+
+      dbConnect
+        .collection("myFirstCollection")
+        .find({}).limit(50)
+        .toArray(function (err, result) {
+          if (err) {
+            res.status(400).send("Error fetching listings!");
+         } else {
+            res.json(result);
+          }
+        });
+    });
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
